1883789faf48bb71715baae35f878979610629e6
Christophe De Troyer <christophe.detroyer@gmail.com>
2026-04-26 13:07:34 +0200

    interop disable by default

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,9 @@
 ExUnit.start()
+# Disable interop test in the CI (docker test for yamux)
+if System.get_env("CI") do
+  ExUnit.configure(exclude: [:interop])
+end
+
 # DB-TESTS-DISABLED: commented to remove database requirements for tests.
 # Re-enable when Ferri.Repo is added back to the supervision tree.
 # Ecto.Adapters.SQL.Sandbox.mode(Ferri.Repo, :manual)
