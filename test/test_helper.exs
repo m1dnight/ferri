1230f3008d0666b3f94003194f02ac0386835993
Christophe De Troyer <christophe.detroyer@gmail.com>
2026-04-26 13:11:53 +0200

    elixir ci

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,8 +1,4 @@
 ExUnit.start()
-# Disable interop test in the CI (docker test for yamux)
-if System.get_env("CI") do
-  ExUnit.configure(exclude: [:interop])
-end
 
 # DB-TESTS-DISABLED: commented to remove database requirements for tests.
 # Re-enable when Ferri.Repo is added back to the supervision tree.
